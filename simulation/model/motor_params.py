01bd38ba04428d753a0db7337848cd1db4d297f9
wildHun7 <lucastefanik@gmail.com>
2026-04-29 20:47:47 +0200

    transfer function + pid

---
+++ simulation/model/motor_params.py
@@ -0,0 +1,8 @@
+# Motor parameters
+
+R = 2.0  # winding resistance [Ohm]
+L = 0.5e-3  # inductance [H]
+Ke = 0.1  # back-EMF constant [V*s/rad]
+Kt = 0.1  # torque constant [N*m/A]
+J = 0.01  # moment of inertia [kg*m^2]
+B = 0.001  # viscous friction coefficient [N*m*s/rad]
