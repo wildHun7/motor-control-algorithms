7967fd8592a132bad867924e414094fda7f8f404
wildHun7 <lucastefanik@gmail.com>
2026-05-11 12:46:45 +0200

    params rename

--- simulation/model/motor_params.py
+++ simulation/model/motor_params.py
@@ -1,8 +1,8 @@
 # Motor parameters
 
-R = 2.0  # winding resistance [Ohm]
-L = 0.5e-3  # inductance [H]
-Ke = 0.1  # back-EMF constant [V*s/rad]
-Kt = 0.1  # torque constant [N*m/A]
-J = 0.01  # moment of inertia [kg*m^2]
-B = 0.001  # viscous friction coefficient [N*m*s/rad]
+R_mot = 2.0  # winding resistance [Ohm]
+L_mot = 0.5e-3  # inductance [H]
+Ke_mot = 0.1  # back-EMF constant [V*s/rad]
+Kt_mot = 0.1  # torque constant [N*m/A]
+J_mot = 0.01  # moment of inertia [kg*m^2]
+B_mot = 0.001  # viscous friction coefficient [N*m*s/rad]
